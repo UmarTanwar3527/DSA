36272506f1fd93a09c020d353a731dcf605becee
Mohammed Umar Tanwar <umartanwar11651913@gmail.com>
2024-02-19 12:09:22 +0530

    CheckIfTheArrayIsSorted

--- DSA-Java-Striver/Arrays/CheckIfTheArrayIsSorted.java
+++ DSA-Java-Striver/Arrays/CheckIfTheArrayIsSorted.java
@@ -1,3 +1,28 @@
 public class CheckIfTheArrayIsSorted {
-    
+    public static int isSorted(int n, int []a) {
+        // Write your code here.
+        for (int i = 1; i < a.length; i++) {
+            if(a[i] < a[i-1]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
+// Leetcode 1752 - Check if The Array is Sorted and Rotated
+class Solution {
+    public boolean check(int[] nums) {
+        int n=nums.length;
+    int c=0;
+    for(int i=0;i<n;i++)
+    {
+        if(nums[(i+1)%n]>=nums[i])
+        {
+
+        }
+        else
+        c++;
+    }
+    return c<=1;
+    }
 }
