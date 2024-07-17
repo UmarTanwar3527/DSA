f879e726087958668386785fd95f6e02916030f5
Mohammed Umar Tanwar <umartanwar11651913@gmail.com>
2024-07-18 01:39:59 +0530

    selection_bubble_insertion_sort_Done

--- DSA-Java-Striver/Sorting Algo/Sorting.java
+++ DSA-Java-Striver/Sorting Algo/Sorting.java
@@ -30,21 +30,34 @@ public class Sorting {
     }
 
     public static void insertion_sort(int arr[], int n) {
-                
+        for(int i = 0; i < n;i++){
+            int j = i;
+            while(j > 0 && arr[j - 1] > arr[j]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+
+                j--;
+            }
+        }
     }
 
     public static void main(String[] args) {
         int arr[] = { 13, 46, 24, 52, 20, 9 };
         int n = arr.length;
+
         System.out.println("Before sorting:");
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
+
+        // CALLING SORTING ALGORITHM
         // selection_sort(arr, n);
         // bubble_sort(arr,n);
         insertion_sort(arr, n);
         
+        // PRINTING SORTED NUMBER
         System.out.println("After sorting:");
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
