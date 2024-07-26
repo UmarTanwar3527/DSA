216f964a599eaa991f491a1d76f12dd99d42efb9
Mohammed Umar Tanwar <umartanwar11651913@gmail.com>
2024-07-26 12:52:53 +0530

    merge sort

--- DSA-Java-Striver/Sorting Algo/Sorting.java
+++ DSA-Java-Striver/Sorting Algo/Sorting.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Sorting {
     public static void selection_sort(int arr[], int n) {
         for (int i = 0; i < n - 1; i++) {
@@ -18,28 +20,73 @@ public class Sorting {
     }
 
     public static void bubble_sort(int arr[], int n) {
-        for(int i = n-1;i>=0;i--){
-            for(int j = 0;j <= i - 1; j++){
-                if(arr[j] > arr[j + 1]){
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
     }
 
     public static void insertion_sort(int arr[], int n) {
-        for(int i = 0; i < n;i++){
+        for (int i = 0; i < n; i++) {
             int j = i;
-            while(j > 0 && arr[j - 1] > arr[j]){
-                int temp = arr[j-1];
-                arr[j-1] = arr[j];
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
                 arr[j] = temp;
 
                 j--;
             }
         }
+    }
+
+    private static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
+        int left = low; // starting index of left half of arr
+        int right = mid + 1; // starting index of right half of arr
+
+        // storing elements in the temporary array in a sorted manner//
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        // if elements on the left half are still left //
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        // if elements on the right half are still left //
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        // transfering all elements from temporary to arr //
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+    }
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low >= high)
+            return;
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid); // left half
+        mergeSort(arr, mid + 1, high); // right half
+        merge(arr, low, mid, high); // merging sorted halves
     }
 
     public static void main(String[] args) {
@@ -55,8 +102,9 @@ public class Sorting {
         // CALLING SORTING ALGORITHM
         // selection_sort(arr, n);
         // bubble_sort(arr,n);
-        insertion_sort(arr, n);
-        
+        // insertion_sort(arr, n);
+        mergeSort(arr, 0, n - 1);
+
         // PRINTING SORTED NUMBER
         System.out.println("After sorting:");
         for (int i = 0; i < n; i++) {
